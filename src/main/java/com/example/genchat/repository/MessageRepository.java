18d3841cf46a40d792ac8627aa041eb0f33cfb85
fathima Af <fathimaameen235@gmail.com>
2026-05-04 19:50:50 +0530

    Removed my GenChat files

--- src/main/java/com/example/genchat/repository/MessageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.genchat.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.genchat.model.Message;
-
-public interface MessageRepository extends JpaRepository<Message, Long> {
-}
--- src/main/java/com/example/genchat/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.genchat.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.genchat.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
